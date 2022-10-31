866925c978e54b1fe00593b59341d4ac73b8dc1f
zy445566 <zyct@vip.qq.com>
2022-10-31 16:02:22 +0800

    feat: fix host

--- src/apps/platform/index.ts
+++ src/apps/platform/index.ts
@@ -1,6 +1,11 @@
 import {VaasServerType, Decorator} from 'vaas-framework'
 import {v1 as uuidV1} from 'uuid'
-import {s3, getAllAppConfigList, isAppNameRegistered, setAppConfigByAppName, getAppConfigDataByName} from './config/app'
+import {
+    s3, 
+    getAllAppConfigList, isAppNameRegistered, setAppConfigByAppName, 
+    getAppConfigDataByName,deleteAppConfigByAppName,
+    getAllHostConfigList, setHostConfig, deleteHostConfigByHost
+} from './config/app'
 import {deploy} from './deploy/index'
 import * as moment from 'moment'
 
@@ -48,6 +53,12 @@ export default class Platform {
     }
 
     @Decorator.VassServer({type:'http',method:'get'})
+    async getAllHostList({req,res}:VaasServerType.HttpParams) {
+        const configData =  await getAllHostConfigList()
+        return {data:configData.map(e=>e.value)}
+    }
+
+    @Decorator.VassServer({type:'http',method:'get'})
     async getAppByName({req,res}:VaasServerType.HttpParams) {
         const {
             appName,
@@ -55,14 +66,35 @@ export default class Platform {
         const appConfigData =  await getAppConfigDataByName({appName})
         return {data:appConfigData.value}
     }
-        
+    
+    @Decorator.VassServer({type:'http',method:'post'})
+    async setHostConfig({req,res}:VaasServerType.HttpParams) {
+        const {
+            appName,
+            host
+        } = req.body
+        const data = await setHostConfig({
+            appName,
+            host
+        })
+        return data;
+    }
+
+    @Decorator.VassServer({type:'http',method:'delete', routerName:"/deleteAppConfig/:appName"})
+    async deleteHostConfig({req,res}:VaasServerType.HttpParams) {
+        let {
+            host
+        } = req.params
+        if(host instanceof Array) {host = host[0]}
+        const data =  await deleteHostConfigByHost({host})
+        return {data}
+    }
 
     @Decorator.VassServer({type:'http',method:'post'})
     async createAppConfig({req,res}:VaasServerType.HttpParams) {
         const {
             appName,
             description,
-            hostList,
             maxWorkerNum,
             allowModuleSet,
             timeout,
@@ -74,7 +106,6 @@ export default class Platform {
         const data = await setAppConfigByAppName({
             appName,
             description,
-            hostList,
             appConfig:{
                 maxWorkerNum,
                 allowModuleSet,
@@ -90,7 +121,6 @@ export default class Platform {
         const {
             appName,
             description,
-            hostList,
             maxWorkerNum,
             allowModuleSet,
             timeout,
@@ -99,7 +129,6 @@ export default class Platform {
         const data = await setAppConfigByAppName({
             appName,
             description,
-            hostList,
             appConfig:{
                 maxWorkerNum,
                 allowModuleSet,
@@ -107,6 +136,16 @@ export default class Platform {
                 resourceLimits
             }
         })
+        return {data}
+    }
+
+    @Decorator.VassServer({type:'http',method:'delete', routerName:"/deleteAppConfig/:appName"})
+    async deleteAppConfig({req,res}:VaasServerType.HttpParams) {
+        let {
+            appName
+        } = req.params
+        if(appName instanceof Array) {appName = appName[0]}
+        const data =  await deleteAppConfigByAppName({appName})
         return {data}
     }
 
