989bcff7afae6c270b33f4bfd9b047d93dcd3808
zy445566 <zyct@vip.qq.com>
2023-07-26 11:55:05 +0800

    feat: 增加类型校验

--- vaas.config.js
+++ vaas.config.js
@@ -1,10 +1,14 @@
+// @ts-check
 const path = require("path");
 const config = require("./build/apps/platform/config/app.js")
-module.exports = {
+const { util } = require("vaas-framework");
+module.exports = util.validVaasConfig({
     appsDir:path.join(__dirname,'build','apps'), 
     port:9080,
     showErrorStack:true,
+    // @ts-ignore 由于是编译后的文件，所以类型不相同
     getAppNameByRequest: config.getAppNameByRequest,
     getAppConfigByAppName:config.getAppConfigByAppName,
+    // @ts-ignore 由于是编译后的文件，所以类型不相同
     getByPassFlowVersion:config.getByPassFlowVersion
-}
+})
